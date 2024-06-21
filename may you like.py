0afb9c06b23b02e86a6a5345dbdabaac7933c5f3
Ahmed Mustafa <42010147.ahmed@acu.edu.eg>
2024-06-21 15:46:26 +0300

    `Update CSV file path for data loading`

--- may you like.py
+++ may you like.py
@@ -12,7 +12,7 @@ app = FastAPI()
 selected_columns = ['Title', 'Tag', 'Review', 'Comment', 'Country', 'Price', 'Rating', 'tags', 'img_link']
 
 # Ensure the CSV file contains the 'img_link' column, or add it if missing
-df = pd.read_csv(r"C:\Users\HP\Downloads\projec_2024\Final model\updated_data_with_img.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/ahmedmostafa147/BackEnd/main/final_data.csv")
 for col in selected_columns:
     if col not in df.columns:
         df[col] = None
